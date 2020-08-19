3f6138045a6bb7c63721ecc85af38370cc2c15e5
PolyanaF <polyanafeitosa60@gmail.com>
2020-08-19 11:26:43 -0300

    Acrescentado mensagem de bom dia e boa tarde e eliminaçao de espaços em branco

--- Course/src/application/Program.java
+++ Course/src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("BOA NOITE!");
+		System.out.println("BOA TARDE");
+		System.out.println("BOM DIA");
 	}
-
 }
